a7ca3431d0358b39be89b463db91add3981d31f3
Carsten Csiky <carsten.csiky@googlemail.com>
2020-02-11 17:25:40 +0100

    WIP: Purty compatible formatting (#8)
    * Add purty to build container
    
    * Format code with purty

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM node:13
 
 ENV PURESCRIPT_DOWNLOAD_SHA256 a78635b85a52f646c02b6a4f0c1d09d114ef75c008cc8523c80b5dd1ffc9fd4dc91e65642383ad8c7d0a2c4f3dd2a4ddc5c8c68eb564fd92adc709c9fcda42e1
 
-RUN yarn global add bower pulp@13.0.0
+RUN yarn global add bower pulp@13.0.0 purty@4.5.2
 
 RUN cd /opt \
     && wget https://github.com/purescript/purescript/releases/download/v0.13.6/linux64.tar.gz \
--- src/Data/Matrix/Reps.purs
+++ src/Data/Matrix/Reps.purs
@@ -6,58 +6,58 @@ import Data.Matrix.Operations
 import Data.Typelevel.Num.Reps (D0, D1, D2, D3)
 import Data.Vec as Vec
 
-empty :: ∀a. Matrix D0 D0 a
+empty :: ∀ a. Matrix D0 D0 a
 empty = Matrix Vec.empty
 
 matrix00 = empty
 
-singleton :: ∀a. a → Matrix D1 D1 a
+singleton :: ∀ a. a -> Matrix D1 D1 a
 singleton x = Vec.singleton x ⤓ Vec.empty ⇥ empty
 
-matrix11 ∷ ∀a. a → Matrix D1 D1 a
+matrix11 :: ∀ a. a -> Matrix D1 D1 a
 matrix11 = singleton
 
-matrix12 ∷ ∀a. a → a → Matrix D1 D2 a
+matrix12 :: ∀ a. a -> a -> Matrix D1 D2 a
 matrix12 x11 x12 = Vec.singleton x11 ⇥ (singleton x12)
 
-matrix13 ∷ ∀a. a → a → a → Matrix D1 D3 a
-matrix13 x11 x12 x13 =
-  Vec.singleton x11 ⇥ Vec.singleton x12 ⇥ (singleton x13)
+matrix13 :: ∀ a. a -> a -> a -> Matrix D1 D3 a
+matrix13 x11 x12 x13 = Vec.singleton x11 ⇥ Vec.singleton x12 ⇥ (singleton x13)
 
-matrix21 ∷ ∀a. a → a → Matrix D2 D1 a
+matrix21 :: ∀ a. a -> a -> Matrix D2 D1 a
 matrix21 x11 x21 =
   Vec.singleton x11
-  ⤓
-  singleton x21
+    ⤓ singleton x21
 
-matrix22 :: ∀a. a → a → a → a → Matrix D2 D2 a
+matrix22 :: ∀ a. a -> a -> a -> a -> Matrix D2 D2 a
 matrix22 x11 x12 x21 x22 =
   matrix12 x11 x12
-  `concatV`
-  matrix12 x21 x22
+    `concatV`
+      matrix12 x21 x22
 
-matrix23 ∷ ∀a. a → a → a → a → a → a → Matrix D2 D3 a
+matrix23 :: ∀ a. a -> a -> a -> a -> a -> a -> Matrix D2 D3 a
 matrix23 x11 x12 x13 x21 x22 x23 =
   matrix13 x11 x12 x13
-  `concatV`
-  matrix13 x21 x22 x23
+    `concatV`
+      matrix13 x21 x22 x23
 
-matrix31 ∷ ∀a. a → a → a → Matrix D3 D1 a
+matrix31 :: ∀ a. a -> a -> a -> Matrix D3 D1 a
 matrix31 x11 x21 x31 =
   matrix21
     x11
     x21
-  `concatV`
-  singleton x31
+    `concatV`
+      singleton x31
 
-matrix32 ∷ ∀a. a → a → a → a → a → a → Matrix D3 D2 a
-matrix32 x11 x12 x21 x22 x31 x32 =
-  matrix31 x11 x21 x31 `concatH` matrix31 x12 x22 x32
+matrix32 :: ∀ a. a -> a -> a -> a -> a -> a -> Matrix D3 D2 a
+matrix32 x11 x12 x21 x22 x31 x32 = matrix31 x11 x21 x31 `concatH` matrix31 x12 x22 x32
 
-matrix33 ∷ ∀a. a → a → a → a → a → a → a → a → a → Matrix D3 D3 a
+matrix33 :: ∀ a. a -> a -> a -> a -> a -> a -> a -> a -> a -> Matrix D3 D3 a
 matrix33 x11 x12 x13 x21 x22 x23 x31 x32 x33 =
-    Vec.vec3 x11 x12 x13
-    ⤓
-    matrix23
-      x21 x22 x23
-      x31 x32 x33
+  Vec.vec3 x11 x12 x13
+    ⤓ matrix23
+        x21
+        x22
+        x23
+        x31
+        x32
+        x33
